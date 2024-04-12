b847c649d4bd3206984725193dd7300520bd33ac
Savanes <146383207+Savanes@users.noreply.github.com>
2024-04-12 22:39:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector('.nav__burger-btn').addEventListener("click", function(){
+    document.querySelector('.nav__burger-btn').classList.toggle('open')
+    document.querySelector('.nav__mobile').classList.toggle('open')
+})
